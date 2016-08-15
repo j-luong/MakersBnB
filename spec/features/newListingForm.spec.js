7ff1caa39678b7b6ddccbfee7f2fb1aa2b72f3b7
JAstbury <jess.astbury@hotmail.com>
2016-08-15 09:55:25 +0100

    weekend updates

--- spec/features/newListingForm.spec.js
+++ spec/features/newListingForm.spec.js
@@ -1,25 +1,27 @@
+process.env.NODE_ENV = 'test';
+
 var Browser = require("Zombie");
 
 Browser.localhost('example.com', 3000);
 
 describe('User visits new listings page', function(){
   var browser = new Browser();
-
-  it('should have a link to a new listings form', function(next){
-    // browser.visit('/users/new', function(err) {
-    //   browser.fill('username', 'user@domain.com');
-    //   browser.fill('password', 'password');
-    //   browser.pressButton('Sign Up!');
-    // });
-    browser.visit('/listings', function(err) {
-      expect(browser.success).toBe(true);
-      // browser.clickLink("Add new.", function(){
-      // expect(browser.location.pathname).toBe("/listings/new");
-      // expect(browser.html("body")).toContain('Add details below...');
-      next();
-    // });
-    // });
-  });
+  //
+  // it('should have a link to a new listings form', function(next){
+  //   // browser.visit('/users/new', function(err) {
+  //   //   browser.fill('username', 'user@domain.com');
+  //   //   browser.fill('password', 'password');
+  //   //   browser.pressButton('Sign Up!');
+  //   // });
+  //   browser.visit('/listings', function(err) {
+  //     expect(browser.success).toBe(true);
+  //     // browser.clickLink("Add new.", function(){
+  //     // expect(browser.location.pathname).toBe("/listings/new");
+  //     // expect(browser.html("body")).toContain('Add details below...');
+  //     next();
+  //   // });
+  //   // });
+  // });
 
   // // it('should have a form to fill in', function(next){
   // //   // browser.visit('/users/new', function(err) {
@@ -37,7 +39,7 @@ describe('User visits new listings page', function(){
   // //     next();
   // //     });
   // //   });
-  });
+  // });
 
 
 });
--- spec/features/signUp.spec.js
+++ spec/features/signUp.spec.js
@@ -5,6 +5,10 @@ Browser.localhost('example.com', 3000);
 describe('User visits signup page', function(){
   var browser = new Browser();
 
+beforeEach(function(){
+  process.env.NODE_ENV='test';
+});
+
   it('should be able to visit sign up page', function(next){
     browser.visit('/users/new', function(err) {
       expect(browser.success).toBe(true);
@@ -18,25 +22,27 @@ describe('User visits signup page', function(){
       next();
     });
   });
+
   it('should have a link to sign up on the homepage', function(next){
-    browser.visit('/', function(err) {
-      browser.clickLink("Sign Up!");
-      expect(browser.location.pathname).toBe("/users/new");
-      next();
+    browser.visit('/').then(function(err) {
+      browser.clickLink("Sign Up", function() {
+        expect(browser.location.pathname).toBe("/users/new");
+        next();
+      });
     });
   });
 
-    it('should have a form to fill in', function(next){
-      browser.visit('/users/new', function(err) {
-        browser.fill('username', 'user1@domain.com');
-        browser.fill('password', 'password');
-        browser.pressButton('Sign Up!', function(){
-        expect(browser.html("body")).toContain('Abode sweet Abode!');
-        expect(browser.location.pathname).toBe("/listings");
-        next();
-        });
+  it('should have a form to fill in', function(next){
+    browser.visit('/users/new', function(err) {
+      browser
+        .fill('username', 'user66@domain.com')
+        .fill('password', 'password')
+        .pressButton('Sign Up!', function(){
+      expect(browser.html("body")).toContain('Abode sweet Abode!');
+      expect(browser.location.pathname).toBe("/listings");
+      next();
       });
     });
-
+  });
 
 });
